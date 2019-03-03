eba86574ace3555a148261c8ed21d64b10ca129d
Joey Wohleb <hwohleb3@gatech.edu>
2019-03-03 08:14:34 -0500

    Removed unused code.

--- src/containers/home-container/index.tsx
+++ src/containers/home-container/index.tsx
@@ -1,16 +1,11 @@
 import { Container, Content, Header, List, ListItem } from 'native-base';
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, TextStyle, ViewStyle } from 'react-native';
+import { Text } from 'react-native';
 import { connect } from 'react-redux';
 import { Action, bindActionCreators, Dispatch } from 'redux';
 
 import { AppState, FlashcardSet } from '../../models';
 import { loadFlashcardSets } from '../../store/sets';
-
-const instructions = Platform.select({
-    android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-    ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-});
 
 interface Props {
     sets: FlashcardSet[];
@@ -28,7 +23,6 @@ class HomeContainer extends Component<Props> {
     }
 
     public render() {
-        console.log(this.props.sets);
         return (
             <Container>
                 <Header />
@@ -45,31 +39,6 @@ class HomeContainer extends Component<Props> {
         );
     }
 }
-
-interface Style {
-    container: ViewStyle;
-    instructions: TextStyle;
-    welcome: TextStyle;
-}
-
-const styles = StyleSheet.create<Style>({
-    container: {
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-        flex: 1,
-        justifyContent: 'center',
-    },
-    instructions: {
-        color: '#333333',
-        marginBottom: 5,
-        textAlign: 'center',
-    },
-    welcome: {
-        fontSize: 20,
-        margin: 10,
-        textAlign: 'center',
-    },
-});
 
 export default connect(
     (state: AppState) => {
